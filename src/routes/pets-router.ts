80e6e3fd7877767e7cb0366b7c597c7c63ffd381
gustavotassi <souzagustavotassi@gmail.com>
2021-08-28 17:54:49 -0300

    feat: implement all pet routes

--- src/routes/pets-router.ts
+++ src/routes/pets-router.ts
@@ -1,14 +1,30 @@
 import express from 'express';
-import { PetService } from '../services/pet-service';
+import { PetService } from '../services';
 
 export const petRouter = express.Router();
 const petService = new PetService();
 
-petRouter.get('/pets', (request, response) => {
-    const { filter } = request.query;
+petRouter.get('/pets/:id', async (request, response) => {
+    const { id } = request.params;
 
     try {
-        const pets = petService.getAllPets(filter);
+        const pet = await petService.getPetById(id);
+
+        return response.json({
+            pet,
+        });
+    } catch (error) {
+        return response.status(500).json({
+            error: 'Error getting pet information',
+        });
+    }
+});
+
+petRouter.get('/pets', async (request, response) => {
+    const { name, species } = request.query;
+
+    try {
+        const pets = await petService.getAllPets(name, species);
 
         return response.json({
             pets,
@@ -16,6 +32,79 @@ petRouter.get('/pets', (request, response) => {
     } catch (error) {
         return response.status(500).json({
             error: 'Error getting pets',
+        });
+    }
+});
+
+petRouter.post('/pets', async (request, response) => {
+    const { name, species, age } = request.body;
+
+    try {
+        const pet = await petService.createPet({
+            name,
+            species,
+            age,
+        });
+
+        return response.json({
+            pet,
+        });
+    } catch (error) {
+        return response.status(500).json({
+            error: 'Failed to create pet',
+        });
+    }
+});
+
+petRouter.put('/pets/update/:id', async (request, response) => {
+    const { id } = request.params;
+    const { name, species, age } = request.body;
+
+    try {
+        const pet = await petService.getPetById(id);
+
+        if (!pet) {
+            return response.status(404).json({
+                error: 'The pet could not be found',
+            });
+        }
+
+        const updatedPet = await petService.updatePet(id, {
+            name,
+            species,
+            age,
+        });
+
+        return response.json({
+            updatedPet,
+        });
+    } catch (error) {
+        return response.status(500).json({
+            error: 'Failed to update pet',
+        });
+    }
+});
+
+petRouter.delete('/pets/delete/:id', async (request, response) => {
+    const { id } = request.params;
+
+    try {
+        const pet = await petService.getPetById(id);
+
+        if (!pet) {
+            return response.status(404).json({
+                error: 'The pet could not be found',
+            });
+        }
+
+        const deletedPet = await petService.deletePet(id);
+
+        return response.json({
+            deletedPet,
+        });
+    } catch (error) {
+        return response.status(500).json({
+            error: 'Failed to delete pet',
         });
     }
 });
--- src/services/pet-service.ts
+++ src/services/pet-service.ts
@@ -6,11 +6,18 @@ export class PetService {
      * Gets all the pets from collection
      * @returns Pets array
      */
-    public async getAllPets(filter?: any): Promise<IPet[]> {
-        let search;
+    public async getAllPets(name?: any, species?: any): Promise<IPet[]> {
+        const search: {
+            name?: any;
+            species?: any;
+        } = {};
 
-        if (filter) {
-            search = { name: new RegExp(filter, 'i') };
+        if (name) {
+            search.name = new RegExp(name, '');
+        }
+
+        if (species) {
+            search.species = new RegExp(species, '');
         }
 
         const pets = await petModel.find({
@@ -43,17 +50,24 @@ export class PetService {
     }
 
     /**
+     * Updates a pet from the collection
+     * @param id The pet identifier
+     * @returns Info about the updated pet
+     */
+    public async updatePet(id: string, pet: IPet) {
+        const updatedPet = await petModel.findByIdAndUpdate(id, pet, {
+            new: true,
+        });
+
+        return updatedPet;
+    }
+
+    /**
      * Deletes a pet from the collection
      * @param id The pet identifier
-     * @returns Info about the deletion
+     * @returns Info about the deleted pet
      */
     public async deletePet(id: string) {
-        const pet = await petModel.findById(id);
-
-        if (!pet) {
-            throw new Error('The pet could not be found');
-        }
-
         const deletedPet = await petModel.findByIdAndDelete(id);
 
         return deletedPet;
