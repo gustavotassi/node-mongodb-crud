addc223519a6678c020daf561a334bb4b236c0d5
gustavotassi <souzagustavotassi@gmail.com>
2021-08-28 16:54:35 -0300

    feat: add create and delete routes

--- src/services/pet-service.ts
+++ src/services/pet-service.ts
@@ -1,3 +1,4 @@
+import { IPet } from '../interfaces';
 import { petModel } from '../models';
 
 export class PetService {
@@ -5,11 +6,56 @@ export class PetService {
      * Gets all the pets from collection
      * @returns Pets array
      */
-    public async getAllPets(filter?: any) {
+    public async getAllPets(filter?: any): Promise<IPet[]> {
+        let search;
+
+        if (filter) {
+            search = { name: new RegExp(filter, 'i') };
+        }
+
         const pets = await petModel.find({
-            name: filter,
+            ...search,
         });
 
         return pets;
+    }
+
+    /**
+     * Finds a pet by its ID
+     * @param id The pet identifier
+     * @returns Info about the found pet
+     */
+    public async getPetById(id: string) {
+        const pet = await petModel.findById(id);
+
+        return pet;
+    }
+
+    /**
+     * Adds a pet to the collection
+     * @param pet Pet to be added
+     * @returns Details of the added pet
+     */
+    public async createPet(pet: IPet): Promise<IPet> {
+        const createdPet = await petModel.create(pet);
+
+        return createdPet;
+    }
+
+    /**
+     * Deletes a pet from the collection
+     * @param id The pet identifier
+     * @returns Info about the deletion
+     */
+    public async deletePet(id: string) {
+        const pet = await petModel.findById(id);
+
+        if (!pet) {
+            throw new Error('The pet could not be found');
+        }
+
+        const deletedPet = await petModel.findByIdAndDelete(id);
+
+        return deletedPet;
     }
 }
