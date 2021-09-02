8cd2230a7d09c3f783267d3bd6ac29f92c9686d4
gustavotassi <souzagustavotassi@gmail.com>
2021-09-01 21:55:44 -0300

    feat: add user name to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,8 +2,9 @@ import mongoose from '../config/database';
 import { IUser } from '../interfaces';
 
 const schema = new mongoose.Schema({
+    name: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false },
     created_at: { type: Date, required: true, default: Date.now },
 });
 
