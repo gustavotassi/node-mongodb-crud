e14df51a7aeae5fdc6dc97920555d1fa98653392
gustavotassi <souzagustavotassi@gmail.com>
2021-09-01 21:26:55 -0300

    feat: add cookie parser and user router

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { json } from 'express';
 import environment from './config/environment';
-import { petRouter } from './routes';
+import { authRouter, petRouter } from './routes';
 
 const app = express();
 const { port } = environment;
 
 app.use(cors());
 app.use(json());
+app.use(cookieParser());
+app.use(authRouter);
 app.use(petRouter);
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
